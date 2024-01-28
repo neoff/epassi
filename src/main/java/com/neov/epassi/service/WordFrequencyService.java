6bc8ed6b05497974552050b129492c4ba4c13b1c
Evgenii Nesterov <en.varg@gmail.com>
2024-01-28 23:49:34 +0200

    remove wrong service and controller

--- src/main/java/com/neov/epassi/service/WordFrequencyService.java
+++
@@ -1,7 +0,0 @@
-package com.neov.epassi.service;
-
-import com.neov.epassi.model.FrequencyResponse;
-
-public interface WordFrequencyService {
-  FrequencyResponse getWordFrequency(String filePath, String kWord, Boolean ignoreCase);
-}
